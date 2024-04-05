e626e8a2a378c0f75d826491a98ff6f12b7c8d8d
Gosudarev Danila <danidagosudarev@gmail.com>
2024-04-05 22:30:01 +0300

    env

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
+import "dotenv/config";
 
 import { registerValidation } from "./validations/auth.js";
-import UserModal from "./models/user.js";
+import checkAuth from "./utils/checkAuth.js";
+import { login, register, getMe } from "./Controllers/UserController.js";
 
 // Подключение к MongoDB
 mongoose
-  .connect(
-    "mongodb+srv://admin:123123123@cluster0.82olslk.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(process.env.DB_CONN)
   .then(() => {
     console.log("DB OK");
   })
@@ -25,56 +22,21 @@ const app = express();
 // Чтение json
 app.use(express.json());
 
+// Логин
+app.post("/auth/login", login);
+
 // Регистрация
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
+app.post("/auth/register", registerValidation, register);
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModal({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось зарегистрироваться",
-    });
-  }
-});
+// Получить инфу о пользователе
+app.get("/auth/me", checkAuth, getMe);
 
 // Слушатель порта 4444
-app.listen(4444, (err) => {
+app.listen(process.env.PORT, (err) => {
   if (err) {
     console.log(err);
   }
   console.log("Server OK");
 });
+
+// 1: 20
